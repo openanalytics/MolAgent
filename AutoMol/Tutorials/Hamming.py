090f787fa5cdf241bdadbb81761ef4454574c207
Joris Tavernier <jtaverni@its.jnj.com>
2025-07-03 13:24:58 +0200

    extracting AutoMol from MolAgent

--- AutoMol/Tutorials/Hamming.py
+++
@@ -1,7 +0,0 @@
-import numpy as np
-class HammingMultioutputScore():   
-    def __call__(self,y_true,y_pred):
-        return -1*np.sum(np.not_equal(y_true, y_pred))/float(y_true.size)
-
-    def __name__(self):
-        return 'HammingMultioutputScore'
